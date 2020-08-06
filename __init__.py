fe1530f54eb1dbe75eb1f2d357064c5fdf00b3d0
Retr0Metal98 <66635436+Retr0Metal98@users.noreply.github.com>
2020-08-06 14:28:15 +0800

    Update __init__.py
    removed lines causing reference loops

--- __init__.py
+++ __init__.py
@@ -2,9 +2,3 @@
 Module used to initialise the file_manipulators package
 '''
 import file_manipulators.exec_paths
-import file_manipulators.common_file_ops
-import file_manipulators.audio_converter
-import file_manipulators.batch_process_ops
-
-import file_manipulators.HyperGAN_sample_manipulator
-import file_manipulators.video_maker
