5833051bfde5554d12258adfe2fbb30d34822e11
Retr0Metal98 <66635436+Retr0Metal98@users.noreply.github.com>
2020-08-06 15:11:21 +0800

    Update __init__.py
    Changed to using "General Store" package structure; accessing functions, variables, etc. can be done with package name + item name

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
 '''
 Module used to initialise the file_manipulators package
 '''
-import file_manipulators.exec_paths
+from .exec_paths import *
+from .common_file_ops import *
+from .audio_converter import *
+from .batch_process_ops import *
