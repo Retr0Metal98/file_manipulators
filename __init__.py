f6dad41b9593c71435a280b68e408ee5a2bbe6e9
Retr0Metal98 <66635436+Retr0Metal98@users.noreply.github.com>
2020-08-11 15:44:57 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,5 +3,5 @@ Module used to initialise the file_manipulators package
 '''
 from .exec_paths import *
 from .common_file_ops import *
-from .audio_converter import *
+from .audio_conv_ops import *
 from .batch_process_ops import *
